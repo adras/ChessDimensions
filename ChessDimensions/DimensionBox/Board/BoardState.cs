a7f1e3a1cf38562c6e96230ab019c5ee4b01062b
Andreas Kurka <andreas.kurka@adesso.de>
2022-03-01 13:38:37 +0100

    Started implementation of a board with pieces

---
+++ ChessDimensions/DimensionBox/Board/BoardState.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DimensionBox.Board
+{
+	internal class BoardState
+	{
+		public static int BOARD_WIDTH = 8;
+		public static int BOARD_HEIGHT = 8;
+		
+		public FieldState[,] fields;
+
+		public BoardState(int width, int height)
+		{
+			fields = new FieldState[width, height];
+		}
+	}
+}
---
+++ ChessDimensions/DimensionBox/Board/ChessPieces.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DimensionBox.Board
+{
+	public enum ChessPieces
+	{
+		None,
+		Pawn,
+		Rook,
+		Horsey,
+		Queen,
+		King,
+	}
+}
